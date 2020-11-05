25f06470d96dbc80873a796dcbb9816584f2ed20
DisguisedJohn <svobodahonza@outlook.com>
2020-11-05 17:29:02 +0100

    Init input

---
+++ PHP11_MxDisplay/mux_7seg_char.vhd
@@ -0,0 +1,101 @@
+----------------------------------------------------------------------------------
+-- Company: VSB-TUO
+-- Engineer: 
+-- 
+-- Create Date:    09:41:26 06/19/2009 
+-- Design Name: 
+-- Module Name:    mux_7seg_char - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity mux_7seg_char is port (
+	rst			: in std_logic ; 			-- asynchronn reset
+	clk			: in std_logic ; 			-- CLOCK input
+	DIN3,DIN2,DIN1,DIN0 : in character ;			 -- std_logic_vector(15 downto 0);		-- data input
+	digit			: out std_logic_vector(3 downto 0);		-- digit drivers
+	seg_out		: out std_logic_vector(7 downto 0));	-- segment drivers
+
+end mux_7seg_char ;
+
+architecture arch_mux_7seg_char of mux_7seg_char is
+
+signal akt_znak		: character ;	-- aktualni pismeno
+signal cd		: std_logic_vector(1 downto 0) ;	-- digit
+signal seg		: std_logic_vector(7 downto 0);	-- segmenty
+
+
+begin
+
+process (CLK, rst)
+begin
+if rst = '1' then
+	digit <= (others => '1') ;
+	cd <= (others => '0') ;
+	akt_znak <= ' ';
+elsif CLK'event and CLK = '1' then
+		cd(1 downto 0) <= cd(1 downto 0) + 1 ;
+	case cd(1 downto 0) is 
+		when "00" =>   akt_znak <= DIN0 ;   digit <= "1110" ; 
+		when "01" =>   akt_znak <= DIN1 ;   digit <= "1101" ; 
+		when "10" =>   akt_znak <= DIN2 ;   digit <= "1011" ; 
+		when others => akt_znak <= DIN3 ;   digit <= "0111" ; 
+	end case ;
+end if ;
+end process ;
+
+--      0
+--     ---  
+--  5 |   | 1
+--     ---   <- 6
+--  4 |   | 2
+--     ---
+--      3
+   
+  with akt_znak SELect
+  seg <= "11111001" when '1',   
+         "10100100" when '2',   
+         "10110000" when '3',   
+         "10011001" when '4',   
+         "10010010" when '5',   
+         "10000010" when '6',   
+         "11111000" when '7',   
+         "10000000" when '8',   
+         "10010000" when '9',   
+			"11000000" when '0',	 
+         "10001000" when 'a',   
+         "10000011" when 'b',   
+         "11000110" when 'c',   
+         "10100001" when 'd',   
+         "10000110" when 'e',   
+         "10001110" when 'f',   
+			"11000010" when 'g',
+			"10001001" when 'h',
+			"11111001" when 'i',
+			"11100001" when 'j',
+			"11000111" when 'l',
+			"10101011" when 'n',
+			"11000000" when 'o',
+			"10001100" when 'p',
+			"10101111" when 'r',
+			"10010010" when 's',
+			"10000111" when 't',
+			"11000001" when 'u',
+			"10010001" when 'y',
+			"10100100" when 'z',
+			"10111111" when '-',
+         "11111111" when others;   -- space
+ 
+  seg_out <= seg;
+ 
+end arch_mux_7seg_char;
+
+
+   
---
+++ PHP11_MxDisplay/php11.vhd
@@ -0,0 +1,147 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    11:32:23 11/25/2009 
+-- Design Name: 
+-- Module Name:    php11 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+---- Uncomment the following library declaration if instantiating
+---- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
+entity php11 is port (
+   clk	: in STD_LOGIC; --- Clock 100kHz
+   btnd	: in STD_LOGIC;
+	
+	
+	an			: out std_logic_vector(3 downto 0);		-- an drivers
+	seg		: out std_logic_vector(7 downto 0)		-- segment drivers
+	);
+end php11;
+ 
+architecture Behavioral of php11 is
+-- Pripojeni entity displeje z m7seg
+ component mux_7seg_char is port (
+	rst			: in std_logic ; 			-- asynchronous reset
+	clk			: in std_logic ; 			-- CLOCK input
+	DIN3,DIN2,DIN1,DIN0 : in character ;			 -- std_logic_vector(15 downto 0);		-- data input
+	digit			: out std_logic_vector(3 downto 0);		-- an drivers
+	seg_out		: out std_logic_vector(7 downto 0));	-- segment drivers
+ end component mux_7seg_char ;
+
+ constant introtxt : string := "dinosaurus";
+ constant CNT1MAX	 : STD_LOGIC_VECTOR(9 downto 0) := "0111111111";	-- 511
+ 
+ -- Timer signals
+ signal clk4Hz : STD_LOGIC;		--timer output
+ signal count	: STD_LOGIC_VECTOR (24 downto 0);	-- timer modulo
+ 
+
+ signal CHAR3, CHAR2, CHAR1, CHAR0 : character;
+ signal CNT1K	 	: STD_LOGIC_VECTOR(9 downto 0);
+ signal CNTTXT		: integer;
+ alias rst			: std_logic is btnd;	-- RESET
+ 
+ constant CNT_WIDTH : integer := 17;
+ 
+ signal CNT : std_logic_vector (CNT_WIDTH-1 downto 0);
+ 
+begin
+	
+	m7seg: mux_7seg_char port map
+	 (	rst	=>	rst,
+		clk	=> CNT(16), 
+		DIN3	=>	CHAR3,
+		DIN2	=>	CHAR2,
+		DIN1	=>	CHAR1,
+		DIN0	=>	CHAR0,
+		digit	=> an,
+		seg_out => seg );
+
+------------------------------------------		
+-- COUNTERS ------------------------------
+
+--  text_pointer: process (clk)
+--	begin
+--	 if clk'event and clk = '1' then
+--	   COUNT <= COUNT + 1;
+--	 end if;>D
+--	end process text_pointer;
+--	
+--	ce1kHz <= '1' when COUNT = x"FFFF" else
+--				 '0';
+
+------------------------------------------
+-- TIMERS ---------------------------------
+ 
+  timer : process (clk)
+	begin
+	 if Rising_Edge (clk) then	-- reakce na nabeznou hranu
+     if count = 24999999 then	-- modulo timeru 25mil > 100k / 25k
+	   count <= (others => '0');	-- vynuluj count
+	   clk4Hz <= not clk4Hz;		-- a invertuj hodnotu clk4Hz
+	   else								-- jinak
+	    count <= count + 1;			-- inkrementuj count
+	  end if;
+    end if;  
+   end process timer;
+
+	
+	
+
+
+
+------------------------------------------					 
+  text_refresh: process (clk)
+   begin
+	 if Rising_Edge (clk) then
+	  if  rst = '1' then
+	   CNTTXT <= 0;
+	  elsif clk4Hz = '1' then
+	   if CNTTXT >= (introtxt'length - 4) then
+		  CNTTXT <= 0;
+		else  
+	     CNTTXT <= CNTTXT + 1;
+		end if;  
+	  end if;
+	 end if; 
+	end process text_refresh;
+  
+  CHAR3 <= introtxt(CNTTXT);
+  CHAR2 <= introtxt(CNTTXT+1);
+  CHAR1 <= introtxt(CNTTXT+2);
+  CHAR0 <= introtxt(CNTTXT+3);
+
+-- counter: process (CLK)
+--	begin
+--	 if CLK'event and CLK = '1' then
+--	   if rst = '1' then
+--	     CNT <= (others => '0');
+--	   else
+--		  CNT <= CNT + 1;
+--	   end if;
+--	  end if;	
+--	end process counter; 	
+
+end Behavioral;
+
+
